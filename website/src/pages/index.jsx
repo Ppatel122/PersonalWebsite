95ef9f947b4fcaff4d9543514d02a285b6245383
Pranj Patel <pranj1012@gmail.com>
2022-02-02 18:03:05 -0700

    Added Photo

---
+++ website/src/pages/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./style.scss";
+import {init} from 'ityped';
+import { useEffect, useRef } from 'react';
+
+const Home = () => {
+
+    const occupation = useRef()
+
+    useEffect(() => {
+        init(occupation.current,{
+            showCursor:true,
+            strings: [" Engineering Student at UofA", " Developer Co-op at Willowglen"]
+        })
+    },[])
+
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="wrapper">
+                    <h1>Hi, I'm Pranj Patel.</h1>
+                    <h1>Software <span ref={occupation}></span></h1>
+                </div>
+            </div>
+            <div className="right">
+                <div className="imgContainer">
+                    <img src="assets/profile.png" alt="Hi"></img>
+                </div>
+            </div>
+
+        </div>
+      );
+  
+};
+
+export default Home;
